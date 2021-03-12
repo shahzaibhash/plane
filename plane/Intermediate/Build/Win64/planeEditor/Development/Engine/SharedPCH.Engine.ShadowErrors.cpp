e17c5553cbe153c07649252e6bd08a239f6e2cd3
Idris <iawardere3@gmail.com>
2021-03-11 19:20:21 -0500

    imported updated local project

--- plane/Intermediate/Build/Win64/planeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/hashm/OneDrive/Documents/GitHub/plane/plane/Intermediate/Build/Win64/planeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- plane/Source/plane/plane.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "plane.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, plane, "plane" );
- 
